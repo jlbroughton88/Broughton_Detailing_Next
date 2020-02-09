d67573bf8f6e13dc54e37f20fccbbd97c567678a
jlb1999 <jlbroughton88@gmail.com>
2020-02-08 20:33:15 -0500

    added now.json

---
+++ .next/static/zduIG8wPeUgEGAEnlM1hR/pages/comps/navbar.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[30],{Mz89:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/comps/navbar",function(){return o("JAUf")}])}},[["Mz89",0,1,20,21,22]]]);
