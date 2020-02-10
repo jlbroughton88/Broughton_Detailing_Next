871d09cc69b8f068de61bd383331b4c713f59cc4
jlb1999 <jlbroughton88@gmail.com>
2020-02-09 21:08:53 -0500

    changed config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const withImages = require("next-images");
 
 module.exports = withImages(
   withSass({
+    exportPathMap: function () {
+        return {
+          '/': { page: '/' },
+          '/about': { page: '/about' },
+          '/services': { page: '/services' },
+          '/work': { page: '/work' },
+          '/contact': { page: '/contact' },
+          '/allclients': { page: '/allclients' }
+        }
+      },
     webpack: function(config) {
       // custom configs
       return config;
